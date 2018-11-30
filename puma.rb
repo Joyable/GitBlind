a30b2c0dbf166b7f207bdb7333de64eec21feeae
Nora Trapp <me@trappdesign.net>
2018-11-30 10:37:21 -0800

    Fix puma config for heroku

--- puma.rb
+++ puma.rb
@@ -1,7 +1,9 @@
-workers 3
-threads 5, 5
+workers Integer(ENV['WEB_CONCURRENCY'] || 3)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
+port        ENV['PORT'] || 3000
 
